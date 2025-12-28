2ccef2715dee2bfbca35d488ca4fd8c0f2ebb0b7
Lyubov Davletshina <chernonosova.lyubov@yandex.ru>
2025-12-28 23:20:53 +0300

    add modul test_services

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -5,6 +5,8 @@ from src.reports import spending_by_weekday, spending_by_category, spending_by_w
 
 
 def test_spending_by_category(df_transactions):
+    """Тест проверяет, что функция корректно возвращает траты по заданной категории
+     за последние три месяца (от переданной даты)."""
     expected_result = spending_by_category(df_transactions, 'Фастфуд', '06.01.2021').reset_index(drop=True)
 
     actual_result = pd.DataFrame({
@@ -19,24 +21,30 @@ def test_spending_by_category(df_transactions):
 
 
 def test_spending_by_category_not_date(df_transactions):
+    """Тест, что при отсутствии транзакций в выбранном периоде(текущая дата) вернется пустой Дата-фрейм"""
     result = spending_by_category(df_transactions, 'Фастфуд')
 
     assert result.empty
 
 
 def test_spending_by_weekday(df_transactions_excel):
+    """Тест проверяет, что функция корректно возвращает средние траты в каждый из
+     дней недели за последние три месяца (от переданной даты)."""
     result = spending_by_weekday(df_transactions_excel, "03.10.2021")
 
     assert result.loc["Monday"] == -466.9661818181818
 
 
 def test_spending_by_weekday_not_date(df_transactions_excel):
+    """Тест, что при отсутствии транзакций в выбранном периоде(текущая дата) вернется пустой Дата-фрейм"""
     result = spending_by_weekday(df_transactions_excel)
 
     assert result.empty
 
 
 def test_spending_by_workday(df_transactions_excel):
+    """Тест проверяет, что функция корректно выводит средние траты в рабочий
+     и в выходной день за последние три месяца (от переданной даты)."""
     result = spending_by_workday(df_transactions_excel, "03.10.2021")
 
     assert result.loc["Выходной"] == -270.8437234042553
@@ -44,6 +52,7 @@ def test_spending_by_workday(df_transactions_excel):
 
 
 def test_spending_by_workday_not_date(df_transactions_excel):
+    """Тест, что при отсутствии транзакций в выбранном периоде(текущая дата) вернется пустой Дата-фрейм"""
     result = spending_by_workday(df_transactions_excel)
 
     assert result.empty
